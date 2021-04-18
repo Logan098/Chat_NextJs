0778698d1f51d0960c150da01eeb1b38908650ac
logan098 <maheshpawar097@gmail.com>
2021-04-18 23:21:52 +0530

    Side bar added

---
+++ components/Sidebar.js
@@ -0,0 +1,73 @@
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import styled from "styled-components";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
+import SearchIcon from "@material-ui/icons/Search";
+
+const Sidebar = () => {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+        <IconContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconContainer>
+      </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats." />
+      </Search>
+
+      <SideBarButton>Start a new chat</SideBarButton>
+
+    </Container>
+  );
+};
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const SideBarButton = styled(Button)`
+width: 100%;
+
+`;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconContainer = styled.div``;
